8f6b0581b4928fc08656ceb1faafa884492fc5e1
ce20480 <aaronvinod25@gmail.com>
2025-03-17 11:41:03 +0000

    tag update

--- config/constants.py
+++ config/constants.py
@@ -12,4 +12,4 @@ GITHUB_REPO = "github.com/ce20480/lilypad-module-sentiment"
 
 # TODO: Specify the GitHub tag, branch, or commit hash.
 # Example: "main" or "c3ed392c11060337cae010862b1af160cd805e67"
-GITHUB_TAG = "a1ae44695bba118062e06736cb392d51637ea981"
+GITHUB_TAG = "4404d0719e91f26fcbe5b42528ead645ee0fa771"
